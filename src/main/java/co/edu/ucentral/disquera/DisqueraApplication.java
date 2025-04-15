d6587df7c9257514aeda150d2598b2b027d4ab42
juan <juan@gmail.com>
2025-04-15 10:46:24 -0500

    Primer commit: creacion de la base de datos

---
+++ src/main/java/co/edu/ucentral/disquera/DisqueraApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.disquera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisqueraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisqueraApplication.class, args);
+	}
+
+}
